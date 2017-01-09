803553df3a109c8681926eb96303d4797cbae876
Daniel Tschinder <daniel.tschinder@researchgate.net>
2017-01-09 16:45:32 +0100

    Initial estree support for ClassMethod

--- src/plugins/flow.js
+++ src/plugins/flow.js
@@ -1052,7 +1052,7 @@ export default function (instance) {
   });
 
   // parse type parameters for class methods
-  instance.extend("parseClassMethod", function () {
+  instance.extend("parseClassMethod", function (inner) {
     return function (classBody, method, isGenerator, isAsync) {
       if (method.variance) {
         this.unexpected(method.variancePos);
@@ -1062,8 +1062,7 @@ export default function (instance) {
       if (this.isRelational("<")) {
         method.typeParameters = this.flowParseTypeParameterDeclaration();
       }
-      this.parseMethod(method, isGenerator, isAsync);
-      classBody.body.push(this.finishNode(method, "ClassMethod"));
+      inner.call(this, classBody, method, isGenerator, isAsync);
     };
   });
 
